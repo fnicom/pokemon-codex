012b5fb705a05ba3c4110585595f230e8337db4f
fnicom <fnicomedes@gmail.com>
2022-05-12 10:15:15 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import PokemonsThumbnails from './components/PokemonsThumbnails';
 
 function App() {
+
+  const [allPokenons, setAllPokemons] = useState([]);
+  const [loadMore, setLoadMore] = useState('https://pokeapi.co/api/v2/pokemon?limit=20');
+
+  const getAllPokemons = async () => {
+    const res = await fetch(loadMore)
+    const data = await res.json()
+
+    setLoadMore(data.next)
+
+    function createPokemonObject (result) {
+      result.forEach( async (pokemon) => {
+          const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
+          const data = await res.json()
+
+          setAllPokemons(currentList => [...currentList, data])
+      });
+    }
+    createPokemonObject(data.results) 
+    await console.log(allPokenons, 'teste')
+
+    await console.log(allPokenons, 'teste 3')
+  } 
+
+  useEffect(() => {
+    getAllPokemons()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+     <h1>Pokemon Evolution</h1>
+     <div className="pokemon-container">
+       <div className="all-container">
+        {allPokenons.map((pokemon, index) => 
+          <PokemonsThumbnails
+          id={pokemon.id}
+          name={pokemon.name}
+          image={pokemon.sprites.other.dream_world.front_default}
+          type={pokemon.types[0].type.name}
+          key={index}
+          />
+          )}
+       </div>
+       <button className="load-more" onClick={() => getAllPokemons()}>Load more</button>
+     </div>
     </div>
   );
 }
